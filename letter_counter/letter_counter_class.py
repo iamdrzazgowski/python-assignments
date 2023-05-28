5ed92d11eb108e2f3e81d68d5901705d4597cb25
iamdrzazgowski <iamdrzazgowski@gmail.com>
2023-05-28 17:52:35 +0200

    letter counter using Class

---
+++ letter_counter/letter_counter_class.py
@@ -0,0 +1,38 @@
+class LetterCounter:
+    def __init__(self, word, char):
+        self.dict = None
+        self.word = word
+        self.char = char
+
+    def CreateDict(self):
+        dict = {}
+        for i in self.word:
+            if i not in dict:
+                dict[i] = 1
+            else:
+                dict[i] += 1
+        self.dict = dict
+
+    def ShowDict(self):
+        return self.dict
+
+    def SingleLetterCounter(self):
+        try:
+            return self.dict[self.char]
+        except:
+            return "Char is not avaible"
+
+    def RemoveUselessKeys(self):
+        uselessKeys = [",", ".", " ", ":"]
+
+        for j in uselessKeys:
+            if j in self.dict:
+                del self.dict[j]
+
+word = str(input("Enter a word: "))
+char = str(input("Enter a char: "))
+l = LetterCounter(word, char)
+l.CreateDict()
+l.RemoveUselessKeys()
+print(l.ShowDict())
+print(l.SingleLetterCounter())
