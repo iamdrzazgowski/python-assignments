012debd87e9b6ab38bc38e47e56cb6c936ebe1b7
iamdrzazgowski <iamdrzazgowski@gmail.com>
2023-05-28 17:14:26 +0200

    test

--- class.py
+++ class.py
@@ -3,13 +3,15 @@ class Test:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def multiply(self):
         return self.x * self.y
+
     def setX(self, x):
         self.x = x
-    
 
-p1 = Test(5,6);
+
+p1 = Test(5, 6);
 print(p1.multiply())
 p1.setX(10)
 print(p1.multiply())
